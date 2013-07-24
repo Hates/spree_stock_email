a0b8a7df9d6c88eca90df21633e86f0f8e5f2e9d
Richard Hart <richard@ur-ban.com>
2013-07-24 13:46:26 +0100

    Emails sent if stock added

---
+++ app/controllers/spree/admin/stock_items_controller_decorator.rb
@@ -0,0 +1,15 @@
+Spree::Admin::StockItemsController.class_eval do
+
+  before_filter :set_current_on_hand_value, only: :create
+  after_filter :send_stock_emails, only: :create
+
+  def set_current_on_hand_value
+    @variant = Spree::Variant.find(params[:variant_id])
+    @had_no_stock = !@variant.in_stock?(1)
+  end
+
+  def send_stock_emails
+    Spree::StockEmail.notify(@variant.product) if @had_no_stock && @variant.in_stock?(1)
+  end
+
+end
